6c2726e239dd2e45c993f370fed3ac04588fe91d
cristian sáez <44517223+cristianS97@users.noreply.github.com>
2023-11-14 21:09:18 -0300

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,18 +1,18 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/cristianS97/jenkinsMavenDocker.git', 'master') { node ->
+            node / gitConfigName('cristian')
+            node / gitConfigEmail('cristian@correo.com')
         }
     }
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('maven-jenkins')
           goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('maven-jenkins')
           goals('test')
         }
         shell('''
